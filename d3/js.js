a180080b29c4f07573caa21b98df41fc14b51fce
Antonio Campos <acamposruiz@gmail.com>
2014-07-07 07:45:20 +0200

    nuevos ejemplos

--- d3/js.js
+++ d3/js.js
@@ -1,3 +1,21 @@
-d3.json('/', function(err, data) {
+d3.json('/gswd3/resources/public/data/service_status.json', function(err, data) {
+    "use strict";
+    d3.select("body")
+        .append("ul")
+        .selectAll("li")
+        .data(data)
+        .enter()
+        .append("li")
+        .text(function(d) {
+            return d.name + ": " + d.status;
+        });
 
+    d3.selectAll("li")
+        .style("font-weight", function(d) {
+            if (d.status == "GOOD SERVICE") {
+                return "normal";
+            } else {
+                return "bold";
+            }
+        })
 });
---
+++ d3_plaza_traffic/js.js
@@ -0,0 +1,28 @@
+d3.json('/gswd3/resources/public/data/plaza_traffic.json', function(err, data) {
+    "use strict";
+    d3.select("body")
+        .append("div")
+        .attr("class", "chart")
+        .selectAll("div.line")
+        .data(data.cash)
+        .enter()
+        .append("div")
+        .attr("class", "line");
+
+    d3.selectAll("div.line")
+        .append("div")
+        .attr("class", "label")
+        .text(function(d) {
+            return d.name
+        });
+
+    d3.selectAll("div.line")
+        .append("div")
+        .attr("class", "bar")
+        .style("width", function(d) {
+            return d.count / 100 + "px"
+        })
+        .text(function(d) {
+            return Math.round(d.count)
+        });
+});
